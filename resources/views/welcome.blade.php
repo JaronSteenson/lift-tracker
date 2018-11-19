cd77be79032d21b37a1214d84d4157db9a969a3f
Jaron Steenson <jaron.steenson@kitomba.com>
2018-11-19 17:22:48 +1300

    Rearrage the project structure slightly to work with my homstead.yaml mapping

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello world</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+    </head>
+    <body>
+
+            <div class="content">
+                <div>
+                    Hello world
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
