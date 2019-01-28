001465a40c7ceedd395a7bf589fb39acc2b72c2a
Jaron Steenson <jaron.steenson@kitomba.com>
2019-01-29 09:56:51 +1300

    Left side bar implementation started

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-                <div class="sidebar-sticky">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <i class="fa fa-plus"></i>
-                            <i class="fa fa-ban"></i>
-                            <a class="nav-link" href="#">
-                                Add a program
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    </div>
+
 @endsection
