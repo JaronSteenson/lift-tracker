3f4dcb356d6a319153fe6f3119e2cc466fde5ac5
Jaron Steenson <jaron.steenson@kitomba.com>
2019-01-31 09:15:15 +1300

    Ensure the side bar and layout look of on all screen sizes (except sm is still having issues)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,15 +46,15 @@
                     </li>
                 @else
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        <a id="nav-dropdown" class="nav-link dropdown-toggle" href="#" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="nav-dropdown">
                             <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
 
@@ -69,7 +69,7 @@
         </ul>
     </div>
 
-    <main class="py-4">
+    <main class="main-content py-4">
         @yield('content')
     </main>
 </div>
