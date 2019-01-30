f5a66d5bccc92fc58be326d84efa481eb8edb431
Jaron Steenson <jaron.steenson@kitomba.com>
2019-01-31 09:27:09 +1300

    ensure both labels have the same sizing rules

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-lg-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-lg-4 col-form-label text-lg-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-sm-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
