980ed2985a28defdae8f9907f97588d68be83b4f
Jaron Steenson <jaron.steenson@kitomba.com>
2019-01-31 09:41:05 +1300

    Move the left side nav bar into a blade parial

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,48 +26,9 @@
 <body>
 <div id="app">
 
-    <div class="navbar navbar-inverse navbar-fixed-left">
-
-        <ul class="nav navbar-nav">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-
-            <ul class="navbar-nav">
-                <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        @if (Route::has('register'))
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        @endif
-                    </li>
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="nav-dropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="nav-dropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-
-        </ul>
-    </div>
+    <nav>
+        @include('layouts.partials.left_nav_bar')
+    </nav>
 
     <main class="main-content py-4">
         @yield('content')
