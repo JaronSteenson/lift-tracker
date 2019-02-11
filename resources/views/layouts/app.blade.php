d498b5cfaac6b3e1943258028b90ca670e22d35a
Jaron Steenson <jaron.steenson@kitomba.com>
2019-02-12 09:01:58 +1300

    Remove unnesicary font call

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,23 +14,18 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
     {{-- Font Awesome--}}
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-
-
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <div id="app">
-
-
     @include('layouts.partials.main_nav_bar')
-
 
     <main class="main-content py-4">
         @yield('content')
