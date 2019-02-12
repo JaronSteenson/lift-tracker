2d7f356d7999c3aac8a929ecfd2ea91625a5046b
Jaron Steenson <jaron.steenson@kitomba.com>
2019-02-13 10:00:32 +1300

    Add application manifest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,28 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+    <!-- App icons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="/app-icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/app-icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/app-icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/app-icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/app-icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/app-icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/app-icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/app-icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/app-icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/app-icons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/app-icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/app-icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/app-icons/favicon-16x16.png">
+
+    <!-- Progressive web app manifest -->
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="{{ config('app.brand_color_dark') }}">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="{{ config('app.brand_color_dark') }}">
 </head>
 <body>
 <div id="app">
