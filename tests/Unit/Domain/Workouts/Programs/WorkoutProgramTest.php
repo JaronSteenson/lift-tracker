64dfbe6048a80ef5fb9397f2eed1850ebd373013
Jaron Steenson <jaron.steenson@kitomba.com>
2019-02-24 22:49:55 +1300

    Add unit tests for workout program and remove example unit test class

---
+++ tests/Unit/Domain/Workouts/Programs/WorkoutProgramTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace LiftTracker\Tests\Unit\Domain\Workouts\Programs;
+
+use LiftTracker\Domain\Workouts\Programs\WorkoutProgram;
+use LiftTracker\User;
+use PHPUnit\Framework\Constraint\Constraint;
+use PHPUnit\Framework\TestCase;
+
+class WorkoutProgramTest extends TestCase
+{
+
+    /**
+     * @param int $attachedUserId
+     * @param int $otherUserId
+     * @param Constraint $assertion
+     * @dataProvider isOwnedByProvider
+     */
+    public function testIsOwnedBy(int $attachedUserId, ?int $otherUserId, Constraint $assertion): void
+    {
+        $workoutProgram = new WorkoutProgram();
+        $workoutProgram->user_id = $attachedUserId;
+
+        $otherUser = new User();
+        $otherUser->id  = $otherUserId;
+
+        $isOwnedBy = $workoutProgram->isOwnedBy($otherUser);
+
+        static::assertThat($isOwnedBy, $assertion);
+    }
+
+    public function isOwnedByProvider(): array
+    {
+        return [
+            'Attached id is the same as other user id' => [123, 123, static::isTrue()],
+            'Attached id differs from the other user id' => [123, 321, static::isFalse()],
+            'The other user id is not set' => [123, null, static::isFalse()],
+        ];
+    }
+
+}
