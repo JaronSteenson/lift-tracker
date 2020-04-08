4b334ba66ffa254c89de0663fe62f13ceee5ca3f
Jaron Steenson <jaron.steenson@totaralearning.com>
2020-04-08 21:43:57 +1200

    trying to git ignore stuff

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+    // .sass('resources/sass/app.scss', 'public/css')
     .webpackConfig({
         plugins: [
             new VuetifyLoaderPlugin({
