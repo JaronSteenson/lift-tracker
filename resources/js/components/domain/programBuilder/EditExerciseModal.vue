182fb4361bc3f21c7dde27957cc1884a8ea63d4e
Jaron Steenson <jaron.steenson@totaralearning.com>
2020-04-19 12:54:24 +1200

    Fix rest period slider on mobile

--- resources/js/components/domain/programBuilder/EditExerciseModal.vue
+++ resources/js/components/domain/programBuilder/EditExerciseModal.vue
@@ -4,45 +4,63 @@
             <v-card>
                 <v-card-title>
                     <VTextField
+                        :autofocus="true"
+                        @blur="isEditingTitle = false"
+                        label="Exercise name"
                         v-if="isEditingTitle"
                         v-model="localState.name"
-                        :autofocus="true"
-                        label="Exercise name"
-                        @blur="isEditingTitle = false"
                     />
-                    <EditableTitle v-else @click="isEditingTitle = true">{{ nameDisplay }}</EditableTitle>
-                    <V-btn icon @click="close">
+                    <EditableTitle @click="isEditingTitle = true" v-else>{{ nameDisplay }}</EditableTitle>
+                    <V-btn @click="close" icon>
                         <v-icon>mdi-close</v-icon>
                     </V-btn>
                 </v-card-title>
                 <v-card-text>
                     <v-container>
                         <VRow>
-                            <VCol cols="12" sm="6" md="6">
+                            <VCol cols="12" md="6" sm="6">
                                 <VTextField
-                                    v-model="localState.weight"
                                     label="Weight (kg)"
                                     type="number"
+                                    v-model="localState.weight"
                                 />
                             </VCol>
-                            <VCol cols="12" sm="6" md="6">
+                            <VCol cols="12" md="6" sm="6">
                                 <VSelect
                                     :items="numberOfSetsOptions"
-                                    v-model="localState.numberOfSets"
                                     label="Number of sets"
+                                    v-model="localState.numberOfSets"
                                 ></VSelect>
                             </VCol>
                         </VRow>
                         <VRow>
-                            <VCol cols="12">
+                            <VCol class="d-flex d-sm-none py-0" cols="12">
+                                <VMessages :value="['Rest period (mins)']" />
+                            </VCol>
+                            <VCol class="d-flex d-sm-none" cols="12">
                                 <VSlider
-                                    v-model="localState.restPeriod"
-                                    prepend-icon="mdi-clock"
-                                    label="Rest period (mins)"
-                                    :min="0"
+                                    class="px-0"
                                     :max="5 * 60"
+                                    :min="0"
+                                    step="15"
+                                    thumb
+                                    ticks
+                                    v-model="localState.restPeriod"
+                                >
+                                    <template v-slot:prepend>
+                                        <span>{{ localState.restPeriod | minsSecDuration }}</span>
+                                    </template>
+                                </VSlider>
+                            </VCol>
+                            <VCol class="d-none d-sm-flex" cols="12">
+                                <VSlider
+                                    :max="5 * 60"
+                                    :min="0"
+                                    label="Rest period (mins)"
+                                    prepend-icon="mdi-clock"
                                     step="15"
                                     ticks
+                                    v-model="localState.restPeriod"
                                 >
                                     <template v-slot:append>
                                         <span>{{ localState.restPeriod | minsSecDuration }}</span>
