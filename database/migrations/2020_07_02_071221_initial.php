ec20e7b108ad2c33f62c11c3ba809a66766e2938
Jaron Steenson <jaron.steenson@totaralearning.com>
2020-07-02 19:37:19 +1200

    Fix ordering of calls

--- database/migrations/2020_07_02_071221_initial.php
+++ database/migrations/2020_07_02_071221_initial.php
@@ -14,6 +14,36 @@ class Initial extends Migration
     public function up()
     {
         DB::unprepared('
+            CREATE TABLE `password_resets`
+            (
+                `email`      varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                `token`      varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                `created_at` timestamp                               NULL DEFAULT NULL,
+                KEY `password_resets_email_index` (`email`)
+            ) ENGINE = InnoDB
+              DEFAULT CHARSET = utf8mb4
+              COLLATE = utf8mb4_unicode_ci;
+        ');
+
+        DB::unprepared('
+            CREATE TABLE `users`
+            (
+                `id`                int unsigned                            NOT NULL AUTO_INCREMENT,
+                `name`              varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                `email`             varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                `email_verified_at` timestamp                               NULL DEFAULT NULL,
+                `password`          varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                `remember_token`    varchar(100) COLLATE utf8mb4_unicode_ci      DEFAULT NULL,
+                `created_at`        timestamp                               NULL DEFAULT NULL,
+                `updated_at`        timestamp                               NULL DEFAULT NULL,
+                PRIMARY KEY (`id`),
+                UNIQUE KEY `users_email_unique` (`email`)
+            ) ENGINE = InnoDB
+              DEFAULT CHARSET = utf8mb4
+              COLLATE = utf8mb4_unicode_ci;
+        ');
+
+        DB::unprepared('
             CREATE TABLE `Exercises`
             (
                 `id`        char(36) COLLATE utf8mb4_unicode_ci     NOT NULL,
@@ -24,6 +54,41 @@ class Initial extends Migration
             ) ENGINE = InnoDB
               DEFAULT CHARSET = utf8mb4
               COLLATE = utf8mb4_unicode_ci;
+        ');
+
+        DB::unprepared('
+            CREATE TABLE `WorkoutPrograms`
+            (
+                `name`      varchar(255) COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
+                `createdAt` timestamp                           NULL DEFAULT NULL,
+                `updatedAt` timestamp                           NULL DEFAULT NULL,
+                `uuid`      char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+                `userId`    int                                      DEFAULT NULL,
+                `id`        int                                 NOT NULL AUTO_INCREMENT,
+                PRIMARY KEY (`id`),
+                UNIQUE KEY `idx_uuid` (`uuid`)
+            ) ENGINE = InnoDB
+              DEFAULT CHARSET = utf8mb4
+              COLLATE = utf8mb4_unicode_ci;
+        ');
+
+        DB::unprepared('
+            CREATE TABLE `WorkoutProgramRoutines`
+            (
+                `uuid`             char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+                `name`             varchar(255)                                                                      DEFAULT NULL,
+                `normalDay`        enum (\'any\',\'Monday\',\'Tuesday\',\'Wensday\',\'Thursday\',\'Friday\',\'Saturday\',\'Sunday\') DEFAULT \'any\',
+                `workoutProgramId` int                                                                               DEFAULT NULL,
+                `createdAt`        timestamp                                                 NULL                    DEFAULT NULL,
+                `updatedAt`        timestamp                                                 NULL                    DEFAULT NULL,
+                `position`         int                                                       NOT NULL,
+                `id`               int                                                       NOT NULL AUTO_INCREMENT,
+                PRIMARY KEY (`id`),
+                UNIQUE KEY `idx_uuid` (`uuid`),
+                KEY `WorkoutProgramRoutines_ibfk_1` (`workoutProgramId`),
+                CONSTRAINT `WorkoutProgramRoutines_ibfk_1` FOREIGN KEY (`workoutProgramId`) REFERENCES `WorkoutPrograms` (`id`) ON DELETE CASCADE
+            ) ENGINE = InnoDB
+              DEFAULT CHARSET = utf8;
         ');
 
         DB::unprepared('
@@ -45,6 +110,28 @@ class Initial extends Migration
                 CONSTRAINT `RoutineExercises_ibfk_1` FOREIGN KEY (`workoutProgramRoutineId`) REFERENCES `WorkoutProgramRoutines` (`id`) ON DELETE CASCADE
             ) ENGINE = InnoDB
               DEFAULT CHARSET = utf8;
+        ');
+
+        DB::unprepared('
+            CREATE TABLE `WorkoutSessions`
+            (
+                `id`                      int unsigned                        NOT NULL AUTO_INCREMENT,
+                `uuid`                    char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+                `workoutProgramRoutineId` int unsigned                             DEFAULT NULL,
+                `userId`                  int unsigned                             DEFAULT NULL,
+                `name`                    varchar(100) COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
+                `startedAt`               timestamp                           NULL DEFAULT NULL,
+                `endedAt`                 timestamp                           NULL DEFAULT NULL,
+                `notes`                   text COLLATE utf8mb4_unicode_ci,
+                `createdAt`               timestamp                           NULL DEFAULT NULL,
+                `updatedAt`               timestamp                           NULL DEFAULT NULL,
+                PRIMARY KEY (`id`),
+                KEY `workoutsessions_uuid_index` (`uuid`),
+                KEY `workoutsessions_workoutprogramroutineid_index` (`workoutProgramRoutineId`),
+                KEY `workoutsessions_userid_index` (`userId`)
+            ) ENGINE = InnoDB
+              DEFAULT CHARSET = utf8mb4
+              COLLATE = utf8mb4_unicode_ci;
         ');
 
         DB::unprepared('
@@ -89,93 +176,6 @@ class Initial extends Migration
                 KEY `sessionsets_sessionexerciseid_foreign` (`sessionExerciseId`),
                 KEY `sessionsets_uuid_index` (`uuid`),
                 CONSTRAINT `sessionsets_sessionexerciseid_foreign` FOREIGN KEY (`sessionExerciseId`) REFERENCES `SessionExercises` (`id`) ON DELETE CASCADE
-            ) ENGINE = InnoDB
-              DEFAULT CHARSET = utf8mb4
-              COLLATE = utf8mb4_unicode_ci;
-        ');
-
-        DB::unprepared('
-            CREATE TABLE `WorkoutProgramRoutines`
-            (
-                `uuid`             char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-                `name`             varchar(255)                                                                      DEFAULT NULL,
-                `normalDay`        enum (\'any\',\'Monday\',\'Tuesday\',\'Wensday\',\'Thursday\',\'Friday\',\'Saturday\',\'Sunday\') DEFAULT \'any\',
-                `workoutProgramId` int                                                                               DEFAULT NULL,
-                `createdAt`        timestamp                                                 NULL                    DEFAULT NULL,
-                `updatedAt`        timestamp                                                 NULL                    DEFAULT NULL,
-                `position`         int                                                       NOT NULL,
-                `id`               int                                                       NOT NULL AUTO_INCREMENT,
-                PRIMARY KEY (`id`),
-                UNIQUE KEY `idx_uuid` (`uuid`),
-                KEY `WorkoutProgramRoutines_ibfk_1` (`workoutProgramId`),
-                CONSTRAINT `WorkoutProgramRoutines_ibfk_1` FOREIGN KEY (`workoutProgramId`) REFERENCES `WorkoutPrograms` (`id`) ON DELETE CASCADE
-            ) ENGINE = InnoDB
-              DEFAULT CHARSET = utf8;
-        ');
-
-        DB::unprepared('
-            CREATE TABLE `WorkoutPrograms`
-            (
-                `name`      varchar(255) COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
-                `createdAt` timestamp                           NULL DEFAULT NULL,
-                `updatedAt` timestamp                           NULL DEFAULT NULL,
-                `uuid`      char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
-                `userId`    int                                      DEFAULT NULL,
-                `id`        int                                 NOT NULL AUTO_INCREMENT,
-                PRIMARY KEY (`id`),
-                UNIQUE KEY `idx_uuid` (`uuid`)
-            ) ENGINE = InnoDB
-              DEFAULT CHARSET = utf8mb4
-              COLLATE = utf8mb4_unicode_ci;
-        ');
-
-        DB::unprepared('
-            CREATE TABLE `WorkoutSessions`
-            (
-                `id`                      int unsigned                        NOT NULL AUTO_INCREMENT,
-                `uuid`                    char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
-                `workoutProgramRoutineId` int unsigned                             DEFAULT NULL,
-                `userId`                  int unsigned                             DEFAULT NULL,
-                `name`                    varchar(100) COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
-                `startedAt`               timestamp                           NULL DEFAULT NULL,
-                `endedAt`                 timestamp                           NULL DEFAULT NULL,
-                `notes`                   text COLLATE utf8mb4_unicode_ci,
-                `createdAt`               timestamp                           NULL DEFAULT NULL,
-                `updatedAt`               timestamp                           NULL DEFAULT NULL,
-                PRIMARY KEY (`id`),
-                KEY `workoutsessions_uuid_index` (`uuid`),
-                KEY `workoutsessions_workoutprogramroutineid_index` (`workoutProgramRoutineId`),
-                KEY `workoutsessions_userid_index` (`userId`)
-            ) ENGINE = InnoDB
-              DEFAULT CHARSET = utf8mb4
-              COLLATE = utf8mb4_unicode_ci;
-        ');
-
-        DB::unprepared('
-            CREATE TABLE `password_resets`
-            (
-                `email`      varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-                `token`      varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-                `created_at` timestamp                               NULL DEFAULT NULL,
-                KEY `password_resets_email_index` (`email`)
-            ) ENGINE = InnoDB
-              DEFAULT CHARSET = utf8mb4
-              COLLATE = utf8mb4_unicode_ci;
-        ');
-
-        DB::unprepared('
-            CREATE TABLE `users`
-            (
-                `id`                int unsigned                            NOT NULL AUTO_INCREMENT,
-                `name`              varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-                `email`             varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-                `email_verified_at` timestamp                               NULL DEFAULT NULL,
-                `password`          varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-                `remember_token`    varchar(100) COLLATE utf8mb4_unicode_ci      DEFAULT NULL,
-                `created_at`        timestamp                               NULL DEFAULT NULL,
-                `updated_at`        timestamp                               NULL DEFAULT NULL,
-                PRIMARY KEY (`id`),
-                UNIQUE KEY `users_email_unique` (`email`)
             ) ENGINE = InnoDB
               DEFAULT CHARSET = utf8mb4
               COLLATE = utf8mb4_unicode_ci;
