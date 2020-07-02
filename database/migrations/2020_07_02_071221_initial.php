00cf0d21c98f4c31731703f99111260d2e0c7ebe
Jaron Steenson <jaron.steenson@totaralearning.com>
2020-07-02 19:22:45 +1200

    Fix class name

--- database/migrations/2020_07_02_071221_initial.php
+++ database/migrations/2020_07_02_071221_initial.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Inital extends Migration
+class Initial extends Migration
 {
     /**
      * Run the migrations.
