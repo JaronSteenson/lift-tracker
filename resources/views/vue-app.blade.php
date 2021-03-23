22bbbba1ab524338a9782da2ff70c65147601d83
Jaron Steenson <jaron.steenson@totaralearning.com>
2021-03-23 15:54:56 +1300

    Better position the splash screen loader/title

--- resources/views/vue-app.blade.php
+++ resources/views/vue-app.blade.php
@@ -33,20 +33,22 @@
         body {
             background: {{ config('app.brand_color_dark') }};
         }
+
         .initial-loading {
-            display: block;
-            margin-top: 40%;
+            margin-top: 40vh;
             text-align: center;
             color: white;
             font-size: 2em;
             font-family: Roboto, sans-serif;
         }
+
         .lds-ellipsis {
             display: inline-block;
             position: relative;
             width: 80px;
             height: 80px;
         }
+
         .lds-ellipsis div {
             position: absolute;
             top: 33px;
@@ -56,22 +58,27 @@
             background: #fff;
             animation-timing-function: cubic-bezier(0, 1, 1, 0);
         }
+
         .lds-ellipsis div:nth-child(1) {
             left: 8px;
             animation: lds-ellipsis1 0.6s infinite;
         }
+
         .lds-ellipsis div:nth-child(2) {
             left: 8px;
             animation: lds-ellipsis2 0.6s infinite;
         }
+
         .lds-ellipsis div:nth-child(3) {
             left: 32px;
             animation: lds-ellipsis2 0.6s infinite;
         }
+
         .lds-ellipsis div:nth-child(4) {
             left: 56px;
             animation: lds-ellipsis3 0.6s infinite;
         }
+
         @keyframes lds-ellipsis1 {
             0% {
                 transform: scale(0);
@@ -80,6 +87,7 @@
                 transform: scale(1);
             }
         }
+
         @keyframes lds-ellipsis3 {
             0% {
                 transform: scale(1);
@@ -88,6 +96,7 @@
                 transform: scale(0);
             }
         }
+
         @keyframes lds-ellipsis2 {
             0% {
                 transform: translate(0, 0);
@@ -102,7 +111,12 @@
 <main id="app">
     <div class="initial-loading">
         <div>{{ config('app.name') }}</div>
-        <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+        <div class="lds-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
     </div>
 </main>
 </body>
