bf87de0be313f9e7b3a9330588256cbda77002c5
Jaron Steenson <jaron.steenson@totaralearning.com>
2021-11-18 12:00:18 +1300

    [0K7D9V0N] Slight tweaks to new landing page

--- resources/js/components/formFields/FacebookLoginButton.vue
+++ resources/js/components/formFields/FacebookLoginButton.vue
@@ -1,6 +1,6 @@
 <template>
     <VBtn class="facebook-login-button white--text" :href="href" color="#4267B2" @click="$emit('click', $event)" :loading="loading">
-        <img class="facebook-login-button__icon" src="/app-icons/facebook-logo-transparent-small.png" alt="">
+        <img class="facebook-login-button__icon" :src="mdiFacebookLogoDataUri" alt="Facebook logo">
         <span class="facebook-login-button__text">Continue with Facebook</span>
     </VBtn>
 </template>
@@ -11,6 +11,11 @@ import { mapState } from 'vuex';
 export default {
     props: {
         loading: Boolean,
+    },
+    data() {
+        return {
+            mdiFacebookLogoDataUri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAQAAABKfvVzAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElNRQfjBBYGJDeRLlUUAAABR0lEQVQ4y5WUvUoDQRSFv2zSpRNUdpOAGAXfwSa1WTAo9lpEC2PhG+Q5FkHQ1iKNIJa+hApaJSoRYxHSJCYeizXuz2x+PAvL3pnz3Zm9w52UiChHhTJFHOCVZ65p8BJxKHhseRoqrqE82YErsLvqapK6cuNATSNN00i1MODOsPuIOwbshM28aV+ryigrJ7Qx2wc8w/6uxVAxAnkC5RMqcyQSgaFyFtukiesOgBXu/f/8U5qKRRlTTwDssWHMlC2KCcAXANmEmaKFw3/kpNSLZEoZlgU6oahn0Z6Rcy0StS0eZgDrkegxwy1boYF66F2iBGxGgBtU0MA4OP/A6sb4QAWLJpdz1+iCJkJ5deZa4VN5YQEtqmiO/Ae0ghat6nvqCiMdxlt0Rx8TgY52zZ5Gts7UN4C+zrWcdAmMoRM1fr+PdaVTLUUdPzATB+2c4R9jAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE5LTA0LTIyVDEzOjM2OjU1LTA3OjAwi4nE0AAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxOS0wNC0yMlQxMzozNjo1NS0wNzowMPrUfGwAAAAASUVORK5CYII=',
+        }
     },
     computed: {
         ...mapState('app', [
@@ -24,7 +29,7 @@ export default {
 
             return `${baseUrl}?&scope=email&client_id=${this.facebookAppId}&redirect_uri=${redirectUrl}&state=csrf-token=${this.csrfToken},after-login-url=${this.afterLoginUrl}`;
         }
-    }
+    },
 }
 </script>
 
--- resources/js/components/pages/LoginPage.vue
+++ resources/js/components/pages/LoginPage.vue
@@ -1,30 +1,32 @@
 <template>
-    <div class="wrap">
-        <div v-if="$vuetify.breakpoint.xsOnly" class="mt-5 d-flex justify-center">
-            <div class="left-column d-flex flex-column justify-center align-center">
-                <h1 class="heading heading--small mx-3">Super simple gym tracking</h1>
-                <img
-                    class="set-overview-demo set-overview-demo--small mt-5 elevation-5"
-                    src="images/set-overview.png"
-                    alt="Set overview"
-                />
-                <FacebookLoginButton class="mt-10"/>
-                <a class="mt-3 d-block" href="/privacy-policy">Privacy policy</a>
-            </div>
-        </div>
-
-        <div v-else class="mt-15 d-flex justify-center">
-            <div class="left-column mr-5 d-flex flex-column justify-center align-center">
-                <h1 class="heading">Super simple gym tracking</h1>
-                <FacebookLoginButton class="mt-5"/>
-                <a class="mt-3 d-block" href="/privacy-policy">Privacy policy</a>
-            </div>
+    <div v-if="$vuetify.breakpoint.xsOnly" class="mt-5 d-flex justify-center">
+        <div class="left-column d-flex flex-column justify-center align-center">
+            <h1 class="heading heading--small mx-3">Super simple gym tracking</h1>
             <img
-                class="set-overview-demo set-overview-demo ml-5 elevation-5"
+                ref="image"
+                class="set-overview-demo set-overview-demo--small mt-5 elevation-5"
+                :class="{ 'set-overview-demo--loaded': showImage }"
                 src="images/set-overview.png"
                 alt="Set overview"
             />
+            <FacebookLoginButton class="mt-10"/>
+            <a class="mt-3 d-block" href="/privacy-policy">Privacy policy</a>
         </div>
+    </div>
+
+    <div v-else class="container-large d-flex justify-center">
+        <div class="left-column mr-5 d-flex flex-column justify-center align-center">
+            <h1 class="heading">Super simple gym tracking</h1>
+            <FacebookLoginButton class="mt-5"/>
+            <a class="mt-3 d-block" href="/privacy-policy">Privacy policy</a>
+        </div>
+        <img
+            ref="image"
+            class="set-overview-demo set-overview-demo ml-5 elevation-5"
+            :class="{ 'set-overview-demo--loaded': showImage }"
+            src="images/set-overview.png"
+            alt="Set overview"
+        />
     </div>
 </template>
 
@@ -35,10 +37,22 @@ export default {
         components: {
             FacebookLoginButton,
         },
+        mounted() {
+            this.$refs.image.onload = () => this.showImage = true;
+        },
+        data() {
+            return {
+                showImage: false,
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+.container-large {
+    margin-top: 15vh;
+}
+
 .left-column {
     max-width: 400px;
 }
@@ -53,6 +67,7 @@ export default {
 }
 
 .set-overview-demo {
+    opacity: 0;
     border-radius: 5px;
 
     $set-overview-demo-lg-scale: 0.6;
@@ -62,6 +77,11 @@ export default {
     &--small {
         height: calc(50vh);
         width: unset;
+    }
+
+    &--loaded {
+        transition: opacity .5s ease-in;
+        opacity: 1;
     }
 }
 
