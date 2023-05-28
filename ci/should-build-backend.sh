b668140838acdf5d024ddbbc16c7922561890c98
Jaron Steenson <jaronsteenson@gmail.com>
2023-05-28 12:35:13 +1200

    Make deploy actually copy the unziped assets to nginx

--- ci/should-build-backend.sh
+++ ci/should-build-backend.sh
@@ -1,1 +1,1 @@
-(git diff HEAD $(. ci/last-success-hash.sh)  --exit-code --name-only {app/**,composer.json,bitbucket-pipelines.yml,**.php}) && echo 'no-changes'
+(git diff HEAD $(. ci/last-success-hash.sh)  --exit-code --name-only {app/**,composer.json,**.php}) && echo 'no-changes'
