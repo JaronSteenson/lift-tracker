b330c57dec527819e604e4183182e316725aeebb
Jaron Steenson <jaron.steenson@kitomba.com>
2025-07-10 22:56:32 +1200

    Trash legacy php

--- ci/should-build-backend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git diff HEAD $(. ci/last-success-hash.sh)  --exit-code --name-only -- {'app/**','tests/**','composer.json','composer.lock','bitbucket-pipelines.yml','**/*.php','LiftTrackerApi/**','LiftTrackerApi.Tests/**'} && noChanges=true;
-if [[ $noChanges ]]; then
-    echo 'No changes since last successful build, skipping build';
-    exit 0;
-else
-    echo 'Changes since last successful build, running build';
-fi;
--- database/factories/WorkoutSessionFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(LiftTracker\Domain\Workouts\Sessions\WorkoutSession::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-    ];
-});
