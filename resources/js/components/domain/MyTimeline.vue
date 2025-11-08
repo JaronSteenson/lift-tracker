09aca908e69aec91c5f6cfd4df1496a9ff06f21c
Jaron Steenson <jaron.steenson@kitomba.com>
2025-11-09 11:24:21 +1300

    Remove some rubbish

--- resources/js/components/domain/MyTimeline.vue
+++ resources/js/components/domain/MyTimeline.vue
@@ -1,169 +1,156 @@
 <template>
-    <div>
-        <h1>Test workout session</h1>
-        <VContainer v-show="showTable">
-            <div class="d-flex justify-space-between align-center mb-4 gap-4">
-                <AddNewButton
-                    class="flex-grow-1"
-                    :to="{ name: 'CheckInEditPage' }"
-                >
-                    Check-in
-                </AddNewButton>
-                <AddNewButton
-                    class="flex-grow-1"
-                    :to="{ name: 'NewSessionRoutineSelectPage' }"
-                    data-testid="startNewSessionButton"
-                >
-                    Session
-                </AddNewButton>
-            </div>
-            <VToolbar :color="toolbarColor" elevation="1">
-                <VToolbarTitle class="text-left">Timeline</VToolbarTitle>
-                <VSwitch
-                    class="mt-4 pt-1 mr-4"
-                    v-model="showTable"
-                    @change="setHomePageShowTable"
-                    :append-icon="$svgIcons.mdiTable"
-                />
-            </VToolbar>
-            <!-- eslint-disable vue/valid-v-slot -->
-            <VDataTable
-                :headers="headers"
-                :items="myWorkoutSessions"
-                :items-per-page="myWorkoutSessions.length"
-                hide-default-footer
-                mobile-breakpoint="0"
+    <VContainer v-show="showTable">
+        <div class="d-flex justify-space-between align-center mb-4 gap-4">
+            <AddNewButton class="flex-grow-1" :to="{ name: 'CheckInEditPage' }">
+                Check-in
+            </AddNewButton>
+            <AddNewButton
+                class="flex-grow-1"
+                :to="{ name: 'NewSessionRoutineSelectPage' }"
+                data-testid="startNewSessionButton"
             >
-                <template v-slot:item.startedAt="{ item: session }">
-                    <RouterLink
-                        :to="{
-                            name: 'SessionOverviewPage',
-                            params: { workoutSessionUuid: session.uuid },
-                        }"
-                    >
-                        {{ getFormattedDate(session) }}
-                    </RouterLink>
-                </template>
-                <template v-slot:item.programName="{ item: session }">
-                    <ProgramName
-                        :workoutProgram="
-                            session.workoutProgramRoutine &&
-                            session.workoutProgramRoutine.workoutProgram
-                        "
-                    />
-                </template>
-                <template v-slot:item.bodyWeight="{ item: session }">
-                    {{ session.bodyWeight ? `${session.bodyWeight}kg` : '' }}
-                </template>
-                <template v-slot:item.duration="{ item: session }">
-                    {{
-                        session.startedAt
-                            ? hoursMinutesSecondsFromStartEnd(
-                                  session.startedAt,
-                                  session.endedAt,
-                              )
-                            : ''
-                    }}
-                </template>
-                <template v-slot:item.menu="{ item: session }">
-                    <VMenu bottom left>
-                        <template v-slot:activator="{ props }">
-                            <VBtn icon flat v-bind="props">
-                                <VIcon>{{ $svgIcons.mdiDotsVertical }}</VIcon>
-                            </VBtn>
-                        </template>
-
-                        <VList>
-                            <VListItem
-                                :to="{
-                                    name: 'SessionOverviewPage',
-                                    params: {
-                                        workoutSessionUuid: session.uuid,
-                                    },
-                                }"
-                            >
-                                <VListItemTitle>View overview</VListItemTitle>
-                            </VListItem>
-                            <VListItem
-                                v-if="
-                                    session.workoutProgramRoutine &&
-                                    session.workoutProgramRoutine.uuid
-                                "
-                                :to="{
-                                    name: 'NewSessionOverviewPage',
-                                    params: {
-                                        originRoutineUuid:
-                                            session.workoutProgramRoutine.uuid,
-                                    },
-                                }"
-                            >
-                                <VListItemTitle>Repeat now</VListItemTitle>
-                            </VListItem>
-                            <VListItem
-                                @click="showDeleteConfirmation(session.uuid)"
-                            >
-                                <VListItemTitle>Delete</VListItemTitle>
-                            </VListItem>
-                        </VList>
-                    </VMenu>
-                </template>
-            </VDataTable>
-            <!-- eslint-enable -->
-            <div v-if="loadingNextPage" class="text-center py-4">
-                <VProgressCircular indeterminate color="primary" />
-                <div class="mt-2 text-body-2 text-medium-emphasis">
-                    Loading more...
-                </div>
-            </div>
-        </VContainer>
-
-        <NarrowContentContainer
-            class="flex-column gap-4"
-            v-show="!showTable"
-            :class="{ 'd-flex': !showTable }"
-        >
-            <div class="d-flex justify-space-between align-center gap-4">
-                <AddNewButton
-                    class="flex-grow-1"
-                    :to="{ name: 'CheckInEditPage' }"
-                >
-                    Check-in
-                </AddNewButton>
-                <AddNewButton
-                    class="flex-grow-1"
-                    :to="{ name: 'NewSessionRoutineSelectPage' }"
-                    data-testid="startNewSessionButton"
-                >
-                    Session
-                </AddNewButton>
-            </div>
-            <VToolbar
-                class="sticky-toolbar"
-                :color="toolbarColor"
-                elevation="1"
-            >
-                <VToolbarTitle class="text-left">Timeline</VToolbarTitle>
-                <VSwitch
-                    class="mt-4 pt-1 mr-4"
-                    v-model="showTable"
-                    @change="setHomePageShowTable"
-                    :append-icon="$svgIcons.mdiTable"
-                />
-            </VToolbar>
-            <SessionStatsCard
-                v-for="workoutSession in myWorkoutSessions"
-                :key="workoutSession.uuid"
-                :workout-session="workoutSession"
-                link-title
+                Session
+            </AddNewButton>
+        </div>
+        <VToolbar :color="toolbarColor" elevation="1">
+            <VToolbarTitle class="text-left">Timeline</VToolbarTitle>
+            <VSwitch
+                class="mt-4 pt-1 mr-4"
+                v-model="showTable"
+                @change="setHomePageShowTable"
+                :append-icon="$svgIcons.mdiTable"
             />
-            <div v-if="loadingNextPage" class="text-center py-4">
-                <VProgressCircular indeterminate color="primary" />
-                <div class="mt-2 text-body-2 text-medium-emphasis">
-                    Loading more...
-                </div>
+        </VToolbar>
+        <!-- eslint-disable vue/valid-v-slot -->
+        <VDataTable
+            :headers="headers"
+            :items="myWorkoutSessions"
+            :items-per-page="myWorkoutSessions.length"
+            hide-default-footer
+            mobile-breakpoint="0"
+        >
+            <template v-slot:item.startedAt="{ item: session }">
+                <RouterLink
+                    :to="{
+                        name: 'SessionOverviewPage',
+                        params: { workoutSessionUuid: session.uuid },
+                    }"
+                >
+                    {{ getFormattedDate(session) }}
+                </RouterLink>
+            </template>
+            <template v-slot:item.programName="{ item: session }">
+                <ProgramName
+                    :workoutProgram="
+                        session.workoutProgramRoutine &&
+                        session.workoutProgramRoutine.workoutProgram
+                    "
+                />
+            </template>
+            <template v-slot:item.bodyWeight="{ item: session }">
+                {{ session.bodyWeight ? `${session.bodyWeight}kg` : '' }}
+            </template>
+            <template v-slot:item.duration="{ item: session }">
+                {{
+                    session.startedAt
+                        ? hoursMinutesSecondsFromStartEnd(
+                              session.startedAt,
+                              session.endedAt,
+                          )
+                        : ''
+                }}
+            </template>
+            <template v-slot:item.menu="{ item: session }">
+                <VMenu bottom left>
+                    <template v-slot:activator="{ props }">
+                        <VBtn icon flat v-bind="props">
+                            <VIcon>{{ $svgIcons.mdiDotsVertical }}</VIcon>
+                        </VBtn>
+                    </template>
+
+                    <VList>
+                        <VListItem
+                            :to="{
+                                name: 'SessionOverviewPage',
+                                params: {
+                                    workoutSessionUuid: session.uuid,
+                                },
+                            }"
+                        >
+                            <VListItemTitle>View overview</VListItemTitle>
+                        </VListItem>
+                        <VListItem
+                            v-if="
+                                session.workoutProgramRoutine &&
+                                session.workoutProgramRoutine.uuid
+                            "
+                            :to="{
+                                name: 'NewSessionOverviewPage',
+                                params: {
+                                    originRoutineUuid:
+                                        session.workoutProgramRoutine.uuid,
+                                },
+                            }"
+                        >
+                            <VListItemTitle>Repeat now</VListItemTitle>
+                        </VListItem>
+                        <VListItem
+                            @click="showDeleteConfirmation(session.uuid)"
+                        >
+                            <VListItemTitle>Delete</VListItemTitle>
+                        </VListItem>
+                    </VList>
+                </VMenu>
+            </template>
+        </VDataTable>
+        <!-- eslint-enable -->
+        <div v-if="loadingNextPage" class="text-center py-4">
+            <VProgressCircular indeterminate color="primary" />
+            <div class="mt-2 text-body-2 text-medium-emphasis">
+                Loading more...
             </div>
-        </NarrowContentContainer>
-    </div>
+        </div>
+    </VContainer>
+
+    <NarrowContentContainer
+        class="flex-column gap-4"
+        v-show="!showTable"
+        :class="{ 'd-flex': !showTable }"
+    >
+        <div class="d-flex justify-space-between align-center gap-4">
+            <AddNewButton class="flex-grow-1" :to="{ name: 'CheckInEditPage' }">
+                Check-in
+            </AddNewButton>
+            <AddNewButton
+                class="flex-grow-1"
+                :to="{ name: 'NewSessionRoutineSelectPage' }"
+                data-testid="startNewSessionButton"
+            >
+                Session
+            </AddNewButton>
+        </div>
+        <VToolbar class="sticky-toolbar" :color="toolbarColor" elevation="1">
+            <VToolbarTitle class="text-left">Timeline</VToolbarTitle>
+            <VSwitch
+                class="mt-4 pt-1 mr-4"
+                v-model="showTable"
+                @change="setHomePageShowTable"
+                :append-icon="$svgIcons.mdiTable"
+            />
+        </VToolbar>
+        <SessionStatsCard
+            v-for="workoutSession in myWorkoutSessions"
+            :key="workoutSession.uuid"
+            :workout-session="workoutSession"
+            link-title
+        />
+        <div v-if="loadingNextPage" class="text-center py-4">
+            <VProgressCircular indeterminate color="primary" />
+            <div class="mt-2 text-body-2 text-medium-emphasis">
+                Loading more...
+            </div>
+        </div>
+    </NarrowContentContainer>
 </template>
 
 <script>
